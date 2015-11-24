d7b4bc83a6c203e26c35060823ebdb953b28471a
Paul E. McKenney <paulmck@linux.vnet.ibm.com>
2015-11-24 15:44:06 -0800

    rcu: Add rcu_normal kernel parameter to suppress expediting
    Although expedited grace periods can be quite useful, and although their
    OS jitter has been greatly reduced, they can still pose problems for
    extreme real-time workloads.  This commit therefore adds a rcu_normal
    kernel boot parameter (which can also be manipulated via sysfs)
    to suppress expedited grace periods, that is, to treat requests for
    expedited grace periods as if they were requests for normal grace periods.
    If both rcu_expedited and rcu_normal are specified, rcu_normal wins.
    This means that if you are relying on expedited grace periods to speed up
    boot, you will want to specify rcu_expedited on the kernel command line,
    and then specify rcu_normal via sysfs once boot completes.
    
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/rcu/tree_exp.h
+++ kernel/rcu/tree_exp.h
@@ -456,6 +456,11 @@ void synchronize_sched_expedited(void)
  	/* If only one CPU, this is automatically a grace period. */	
 	if (rcu_blocking_is_gp())	
 		return;	
+	/* If expedited grace periods are prohibited, fall back to normal. */
+	if (rcu_gp_is_normal()) {
+		wait_rcu_gp(call_rcu_sched);
+		return;
+	}
  	/* Take a snapshot of the sequence number.  */	
 	s = rcu_exp_gp_seq_snap(rsp);	
  	rnp = exp_funnel_lock(rsp, s);	
@@ -523,6 +528,11 @@ void synchronize_rcu_expedited(void)
 	struct rcu_node *rnp_unlock;	
 	struct rcu_state *rsp = rcu_state_p;	
 	unsigned long s;	
+	/* If expedited grace periods are prohibited, fall back to normal. */
+	if (rcu_gp_is_normal()) {
+		wait_rcu_gp(call_rcu);
+		return;
+	}
  	s = rcu_exp_gp_seq_snap(rsp);	
  	rnp_unlock = exp_funnel_lock(rsp, s);	
 	if (rnp_unlock == NULL)	
