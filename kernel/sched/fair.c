807e241fc421350a75236b0ff481fb938e11d900
Peter Zijlstra <peterz@infradead.org>
2016-06-17 11:20:46 +0200

    sched/fair: Fix PELT integrity for new groups
    Vincent reported that when a new task is moved into a new cgroup it
    gets attached twice to the load tracking:
    
      sched_move_task()
        task_move_group_fair()
          detach_task_cfs_rq()
          set_task_rq()
          attach_task_cfs_rq()
            attach_entity_load_avg()
              se->avg.last_load_update = cfs_rq->avg.last_load_update // == 0
    
      enqueue_entity()
        enqueue_entity_load_avg()
          update_cfs_rq_load_avg()
            now = clock()
            __update_load_avg(&cfs_rq->avg)
              cfs_rq->avg.last_load_update = now
              // ages load/util for: now - 0, load/util -> 0
          if (migrated)
            attach_entity_load_avg()
              se->avg.last_load_update = cfs_rq->avg.last_load_update; // now != 0
    
    The problem is that we don't update cfs_rq load_avg before all
    entity attach/detach operations. Only enqueue_task() and migrate_task()
    do this.
    
    By fixing this, the above will not happen, because the
    sched_move_task() attach will have updated cfs_rq's last_load_update
    time before attach, and in turn the attach will have set the entity's
    last_load_update stamp.
    
    Note that there is a further problem with sched_move_task() calling
    detach on a task that hasn't yet been attached; this will be taken
    care of in a subsequent patch.
    
    Reported-by: Vincent Guittot <vincent.guittot@linaro.org>
    Tested-by:  Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Yuyang Du <yuyang.du@intel.com>
    Cc: linux-kernel@vger.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3644,6 +3644,12 @@ update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq, bool update_freq)
 #define SKIP_AGE_LOAD	0x0
 #define SKIP_CPUFREQ	0x0
 
+static inline int
+update_cfs_rq_load_avg(u64 now, struct cfs_rq *cfs_rq, bool update_freq)
+{
+	return 0;
+}
+
 static inline void update_load_avg(struct sched_entity *se, int not_used1){}
 static inline void
 enqueue_entity_load_avg(struct cfs_rq *cfs_rq, struct sched_entity *se) {}
@@ -10725,6 +10731,7 @@ static void detach_task_cfs_rq(struct task_struct *p)
 {
 	struct sched_entity *se = &p->se;
 	struct cfs_rq *cfs_rq = cfs_rq_of(se);
+	u64 now = cfs_rq_clock_task(cfs_rq);
 
 	if (!vruntime_normalized(p)) {
 		/*
@@ -10735,6 +10742,7 @@ static void detach_task_cfs_rq(struct task_struct *p)
 		se->vruntime -= cfs_rq->min_vruntime;
 	}
 
+	update_cfs_rq_load_avg(now, cfs_rq, false);
 	detach_entity_cfs_rq(se);
 }
 
@@ -10742,7 +10750,9 @@ static void attach_task_cfs_rq(struct task_struct *p)
 {
 	struct sched_entity *se = &p->se;
 	struct cfs_rq *cfs_rq = cfs_rq_of(se);
+	u64 now = cfs_rq_clock_task(cfs_rq);
 
+	update_cfs_rq_load_avg(now, cfs_rq, false);
 	attach_entity_cfs_rq(se);
 
 	if (!vruntime_normalized(p))
