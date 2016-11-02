98d2932892c491f00c09e3afa5db73b7225073bb
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2016-11-02 17:30:02 +0100

    rcu: update: Make RCU_EXPEDITE_BOOT be the default
    RCU_EXPEDITE_BOOT should speed up the boot process by enforcing
    synchronize_rcu_expedited() instead of synchronize_rcu() during the boot
    process. There should be no reason why one does not want this and there
    is no need worry about real time latency at this point.
    Therefore make it default.
    
    Note that users wishing to avoid expediting entirely, for example when
    bringing up new hardware possibly having flaky IPIs, can use the
    rcu_normal boot parameter to override boot-time expediting.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    [ paulmck: Reworded commit log. ]
    Signed-off-by: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Reviewed-by: Josh Triplett <josh@joshtriplett.org>

--- kernel/rcu/update.c
+++ kernel/rcu/update.c
@@ -128,8 +128,7 @@ bool rcu_gp_is_normal(void)
 	return READ_ONCE(rcu_normal);
 }
 
-static atomic_t rcu_expedited_nesting =
-	ATOMIC_INIT(IS_ENABLED(CONFIG_RCU_EXPEDITE_BOOT) ? 1 : 0);
+static atomic_t rcu_expedited_nesting = ATOMIC_INIT(1);
 
 /*
  * Should normal grace-period primitives be expedited?  Intended for
@@ -179,8 +178,7 @@ EXPORT_SYMBOL_GPL(rcu_unexpedite_gp);
  */
 void rcu_end_inkernel_boot(void)
 {
-	if (IS_ENABLED(CONFIG_RCU_EXPEDITE_BOOT))
-		rcu_unexpedite_gp();
+	rcu_unexpedite_gp();
 	if (rcu_normal_after_boot)
 		WRITE_ONCE(rcu_normal, 1);
 }
