21ac57724558182cac2782885df07d75cfc065ca
Leo Yan <leo.yan@linaro.org>
2016-12-22 23:58:51 +0800

    sched/fair: kick active load balance for misfit task
    Current code checks destination CPU is overutilized or not before kick
    active load balance and if the destination CPU is overutilized the
    function will bail out without kicking active balance.
    
    This patch is more aggressively to kick active balance if find source
    CPU has single misfit task and destination CPU is in idle balance. So
    this can quickly migrate the misfit task to higher capacity CPU.
    
    Change-Id: I5ee8a654a88f6600ad43fb6229d78c29d6ee9822
    Signed-off-by: Leo Yan <leo.yan@linaro.org>
    Patch-mainline: eas-dev @ 12/22/16, 15:58
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9877,6 +9877,11 @@ static int need_active_balance(struct lb_env *env)
 			return 1;
 	}
 
+        if ((env->idle != CPU_NOT_IDLE) &&
+            (capacity_orig_of(env->src_cpu) < capacity_orig_of(env->dst_cpu)) &&
+            env->src_rq->cfs.h_nr_running == 1 && env->src_rq->misfit_task)
+                return 1;
+
 	return unlikely(sd->nr_balance_failed >
 			sd->cache_nice_tries + NEED_ACTIVE_BALANCE_THRESHOLD);
 }
