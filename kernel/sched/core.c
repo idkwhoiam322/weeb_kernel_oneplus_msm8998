820e23bc7d7cad7ce5c49bd3735d6cbba296794a
Peter Zijlstra <peterz@infradead.org>
2017-02-21 14:36:23 +0100

    sched/core: Make sched_ttwu_pending() atomic in time
    Since all tasks on the wake_list are woken under a single rq->lock
    avoid calling update_rq_clock() for each task.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1887,7 +1887,7 @@ ttwu_do_activate(struct rq *rq, struct task_struct *p, int wake_flags,
 		rq->nr_uninterruptible--;
 #endif
 
-	ttwu_activate(rq, p, ENQUEUE_WAKEUP | ENQUEUE_WAKING);
+	ttwu_activate(rq, p, ENQUEUE_WAKEUP | ENQUEUE_WAKING | ENQUEUE_NOCLOCK);
 	ttwu_do_wakeup(rq, p, wake_flags, rf);
 }
 
@@ -1927,6 +1927,7 @@ void sched_ttwu_pending(void)
 		return;
 
 	rq_lock_irqsave(rq, &rf);
+	update_rq_clock(rq);
 
 	while (llist) {
 		p = llist_entry(llist, struct task_struct, wake_entry);
@@ -2031,6 +2032,7 @@ static void ttwu_queue(struct task_struct *p, int cpu)
 #endif
 
 	rq_lock(rq, &rf);
+	update_rq_clock(rq);
         ttwu_do_activate(rq, p, 0, &rf);
 	rq_unlock(rq, &rf);
 }
