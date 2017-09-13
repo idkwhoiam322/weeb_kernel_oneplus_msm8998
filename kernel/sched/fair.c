ad243607d9ff82d86aeb6141750c2cac51070868
Uladzislau Rezki <urezki@gmail.com>
2017-09-13 12:24:30 +0200

    sched/fair: Search a task from the tail of the queue
    As a first step this patch makes cfs_tasks list as MRU one.
    It means, that when a next task is picked to run on physical
    CPU it is moved to the front of the list.
    
    Therefore, the cfs_tasks list is more or less sorted (except
    woken tasks) starting from recently given CPU time tasks toward
    tasks with max wait time in a run-queue, i.e. MRU list.
    
    Second, as part of the load balance operation, this approach
    starts detach_tasks()/detach_one_task() from the tail of the
    queue instead of the head, giving some advantages:
    
     - tends to pick a task with highest wait time;
    
     - tasks located in the tail are less likely cache-hot,
       therefore the can_migrate_task() decision is higher.
    
    hackbench illustrates slightly better performance. For example
    doing 1000 samples and 40 groups on i5-3320M CPU, it shows below
    figures:
    
     default: 0.657 avg
     patched: 0.646 avg
    
    Signed-off-by: Uladzislau Rezki (Sony) <urezki@gmail.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Kirill Tkhai <tkhai@yandex.ru>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Mike Galbraith <umgwanakikbuti@gmail.com>
    Cc: Nicolas Pitre <nicolas.pitre@linaro.org>
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Oleksiy Avramchenko <oleksiy.avramchenko@sonymobile.com>
    Cc: Paul Turner <pjt@google.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Tim Chen <tim.c.chen@linux.intel.com>
    Link: http://lkml.kernel.org/r/20170913102430.8985-2-urezki@gmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    [ab123321]
    Fix conflict caused by 4c6a82485fddd56037c20c08d5b4c6fd6b8fa31c
    
    Signed-off-by: Artem Labazov <123321artyom@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7343,12 +7343,7 @@ again:
 		set_next_entity(cfs_rq, se);
 	}
 
-	if (hrtick_enabled(rq))
-		hrtick_start_fair(rq, p);
-
-	rq->misfit_task = !task_fits_max(p, rq->cpu);
-
-	return p;
+	goto done;
 simple:
 	cfs_rq = &rq->cfs;
 #endif
@@ -7365,6 +7360,16 @@ simple:
 	} while (cfs_rq);
 
 	p = task_of(se);
+
+done: __maybe_unused
+#ifdef CONFIG_SMP
+	/*
+	 * Move the next running task to the front of
+	 * the list, so our cfs_tasks list becomes MRU
+	 * one.
+	 */
+	list_move(&p->se.group_node, &rq->cfs_tasks);
+#endif
 
 	if (hrtick_enabled(rq))
 		hrtick_start_fair(rq, p);
@@ -7823,11 +7828,12 @@ static void detach_task(struct task_struct *p, struct lb_env *env)
  */
 static struct task_struct *detach_one_task(struct lb_env *env)
 {
-	struct task_struct *p, *n;
+	struct task_struct *p;
 
 	lockdep_assert_held(&env->src_rq->lock);
 
-	list_for_each_entry_safe(p, n, &env->src_rq->cfs_tasks, se.group_node) {
+	list_for_each_entry_reverse(p,
+			&env->src_rq->cfs_tasks, se.group_node) {
 		if (!can_migrate_task(p, env))
 			continue;
 
@@ -7883,7 +7889,7 @@ static int detach_tasks(struct lb_env *env)
 				env->dst_rq->nr_running > 0)
 			break;
 
-		p = list_first_entry(tasks, struct task_struct, se.group_node);
+		p = list_last_entry(tasks, struct task_struct, se.group_node);
 
 		env->loop++;
 		/* We've more or less seen every task there is, call it quits */
@@ -7934,7 +7940,7 @@ static int detach_tasks(struct lb_env *env)
 
 		continue;
 next:
-		list_move_tail(&p->se.group_node, tasks);
+		list_move(&p->se.group_node, tasks);
 	}
 
 	/*
