ce61775eba943283851d5b9a41b9055c8f8f391e
Chris Redpath <chris.redpath@arm.com>
2017-12-19 19:32:02 +0000

    ANDROID: sched: fair: Bypass energy-aware wakeup for prefer-idle tasks
    Use the upstream slow path to find an idle cpu for prefer-idle tasks.
    This slow-path is actually faster than the EAS path we are currently
    going through (select_energy*) which is really slow.
    
    No performance degradation is seen with this and it reduces the delta
    quite a bit between upstream and out of tree code.
    
    It's not clear yet if using the mainline slow path task placement when
    a task has the schedtune attribute prefer_idle=1 is the right thing to
    do for products. Put the option to disable this behind a sched feature
    so we can try out both options.
    
    Change-Id: Icf762a101c92c0e3f9e61df0370247fa15455581
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    (refactored for 4.14 version)
    Signed-off-by: Chris Redpath <chris.redpath@arm.com>
    idkwhoiam322: Port to 4.4
    Cherry-picked from: https://android.googlesource.com/kernel/msm.git/+/c0ff131c88f68e4985793663144b6f9cf77be9d3
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7632,7 +7632,16 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 
 	rcu_read_lock();
 	sd = rcu_dereference(cpu_rq(prev_cpu)->sd);
-	if (energy_aware() && sd && !sd_overutilized(sd)) {
+	if (energy_aware() && sd && !sd_overutilized(sd) &&
+		(sched_feat(EAS_PREFER_IDLE) && 
+			!(schedtune_prefer_idle(p) > 0 && !sync))) {
+		/*
+		 * If the sync flag is set but ignored, prefer to
+		 * select cpu in the same cluster as current. So
+		 * if current is a big cpu and sync is set, indicate
+		 * that the selection algorithm for a boosted task
+		 * should be used.
+		 */
 		bool sync_boost = sync && cpu >= start_cpu(true);
 
 		new_cpu = select_energy_cpu_brute(p, prev_cpu, sync_boost);
