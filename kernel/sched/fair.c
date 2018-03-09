9c4821a7482e05cfc87d32710043717b19d489db
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2018-03-09 14:28:00 -0800

    sched/fair: Add missing update_rq_clock() call while pushing task
    rq->clock_update_flags < RQCF_ACT_SKIP warning is seen while pushing
    a task from a misfit_task runqueue.
    
    deactivate_task+0x298/0x2fc
    detach_task+0x4c/0x120
    active_load_balance_cpu_stop+0x254/0x580
    cpu_stopper_thread+0xb8/0x13c
    smpboot_thread_fn+0x1c8/0x25c
    kthread+0x108/0x134
    ret_from_fork+0x10/0x18
    
    The locking of busiest_rq resets the RQCF_UPDATED flag. The code path
    in active_load_balance_cpu_stop() fails to update the rq clock before
    moving the 'push_task'.
    
    Update the rq clock just before moving the push task.
    
    Change-Id: Id0e7d7a5446775fbbbfca9fe5307240f675884e5
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10015,6 +10015,7 @@ static int active_load_balance_cpu_stop(void *data)
 	if (push_task) {
 		if (task_on_rq_queued(push_task) &&
 				task_cpu(push_task) == busiest_cpu) {
+                        update_rq_clock(busiest_rq);
 			detach_task(push_task, &env);
 			push_task_detached = 1;
 		}
