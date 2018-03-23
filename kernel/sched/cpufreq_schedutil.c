7222b4036f77c281d6fa47d1985f9743d374e06a
Jonathan Avila <avilaj@codeaurora.org>
2018-03-23 15:32:40 -0700

    cpufreq: schedutil: Only apply single core busy logic with PELT
    Currently, sugov_update_single will apply its busy logic, keeping frequency
    high.  However, this is only required for PELT, not WALT; remove this
    logic if PELT is not in use.
    
    Change-Id: Ia0d9523d683568e373fa0d459eecbd3fae78fcff
    Signed-off-by: Jonathan Avila <avilaj@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -312,7 +312,7 @@ static void sugov_update_single(struct update_util_data *hook, u64 time,
 	if (!sugov_should_update_freq(sg_policy, time))
 		return;
 
-	busy = sugov_cpu_is_busy(sg_cpu);
+	busy = use_pelt() && sugov_cpu_is_busy(sg_cpu);
 
 	if (flags & SCHED_CPUFREQ_DL) {
 		next_f = policy->cpuinfo.max_freq;
