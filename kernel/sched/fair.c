f884db6c456037dea27145a01ab42b02cfb284de
Rick Yiu <rickyiu@google.com>
2018-05-18 11:54:10 +0800

    BACKPORT: sched/fair: remove sync logic from select_energy_cpu_brute
    This patch is originated from wahoo-4.4 kernel,
    commit 9767a33691cb ("Move sync logic out of select_energy_cpu_brute")
    but only part of it, since sync logic is already under
    select_task_rq_fair().
    
    bug: 78790904
    Change-Id: I22e27cc3b6218e4eff10847af4d9cce93a99cf38
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    [yaro]: reworked a bit for 4.4
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7184,7 +7184,7 @@ static int wake_cap(struct task_struct *p, int cpu, int prev_cpu)
 	return min_cap * 1024 < task_util_est(p) * capacity_margin;
 }
 
-static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync)
+static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu)
 {
 	struct sched_domain *sd;
 	int target_cpu = prev_cpu, tmp_target, tmp_backup;
@@ -7192,16 +7192,6 @@ static int select_energy_cpu_brute(struct task_struct *p, int prev_cpu, int sync
 
 	schedstat_inc(p, se.statistics.nr_wakeups_secb_attempts);
 	schedstat_inc(this_rq(), eas_stats.secb_attempts);
-
-	if (sysctl_sched_sync_hint_enable && sync) {
-		int cpu = smp_processor_id();
-
-		if (cpumask_test_cpu(cpu, tsk_cpus_allowed(p))) {
-			schedstat_inc(p, se.statistics.nr_wakeups_secb_sync);
-			schedstat_inc(this_rq(), eas_stats.secb_sync);
-			return cpu;
-		}
-	}
 
 	rcu_read_lock();
 #ifdef CONFIG_CGROUP_SCHEDTUNE
@@ -7314,7 +7304,7 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 	}
 
 	if (energy_aware() && !(cpu_rq(prev_cpu)->rd->overutilized))
-		return select_energy_cpu_brute(p, prev_cpu, sync);
+		return select_energy_cpu_brute(p, prev_cpu);
 
 	rcu_read_lock();
 	for_each_domain(cpu, tmp) {
@@ -10620,7 +10610,7 @@ void check_for_migration(struct rq *rq, struct task_struct *p)
 		    rq->curr->nr_cpus_allowed == 1)
 			return;
 
-		new_cpu = select_energy_cpu_brute(p, cpu, 0);
+		new_cpu = select_energy_cpu_brute(p, cpu);
 		if (capacity_orig_of(new_cpu) > capacity_orig_of(cpu)) {
 			active_balance = kick_active_balance(rq, p, new_cpu);
 			if (active_balance)
