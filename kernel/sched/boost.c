3acd3d8d78423ee20ac0d2ee86a4858f3c25afe8
Wei Wang <wvw@google.com>
2018-05-25 21:38:50 -0700

    sched: do not return error when set the same sched_boost value
    Only return error when switching from one boost type to another.
    
    Bug: 80322824
    Test: no error set same value to /proc/sys/kernel/sched_boost several
          times
    Test: error when set "1","2" to /proc/sys/kernel/sched_boost alternately
          to change mode without set "0" first
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: Id6b291cfd932e1f7f69b519edcc8e7e57df569b1

--- kernel/sched/boost.c
+++ kernel/sched/boost.c
@@ -59,8 +59,14 @@ int sched_boost_handler(struct ctl_table *table, int write,
 		else
 			reset_stune_boost("top-app", boost_slot);
 	} else {
-		*data = old_val;
-		ret = -EINVAL;
+		/*
+		 * Only return error when switching from one boost type
+		 * to another.
+		 */
+		if (old_val != *data) {
+			*data = old_val;
+			ret = -EINVAL;
+		}
 	}
 #endif // CONFIG_DYNAMIC_STUNE_BOOST
 
