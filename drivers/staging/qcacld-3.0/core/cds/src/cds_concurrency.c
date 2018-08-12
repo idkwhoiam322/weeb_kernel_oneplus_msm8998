63c2d4e2bc0727d974dfb015d7754baccc39d7eb
Sultan Alsawaf <sultan@kerneltoast.com>
2018-08-11 18:13:12 -0700

    treewide: Fix code issues detected using GCC 8.2.0
    Use the latest version of GCC to take advantage of improved static analysis.
    These issues appeared as warnings from the compiler.
    
    Many of these fixes are for clearly incorrect code; compiler warnings
    should not be taken lightly.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/staging/qcacld-3.0/core/cds/src/cds_concurrency.c
+++ drivers/staging/qcacld-3.0/core/cds/src/cds_concurrency.c
@@ -5576,7 +5576,6 @@ static QDF_STATUS cds_modify_pcl_based_on_enabled_channels(
 			weight_list_org[pcl_len++] = weight_list_org[i];
 		}
 	}
-	*pcl_len_org = pcl_len;
 
 	return QDF_STATUS_SUCCESS;
 }
@@ -10130,7 +10129,6 @@ QDF_STATUS cds_modify_sap_pcl_based_on_mandatory_channel(uint8_t *pcl_list_org,
 			weight_list_org[pcl_len++] = weight_list_org[i];
 		}
 	}
-	*pcl_len_org = pcl_len;
 
 	return QDF_STATUS_SUCCESS;
 }
