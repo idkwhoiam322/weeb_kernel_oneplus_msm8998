c988be0745611cb03f0d0052c8705e10c296fc1e
celtare21 <celtare21@gmail.com>
2018-09-18 11:08:21 +0000

    Revert "scsi: ufs: Change power mode on line reset"
    This reverts commit 492d614a96f2990ba9259f3352d7f58eec6a28d6.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/scsi/ufs/ufshcd.h
+++ drivers/scsi/ufs/ufshcd.h
@@ -855,7 +855,6 @@ struct ufs_hba {
 	/* Work Queues */
 	struct work_struct eh_work;
 	struct work_struct eeh_work;
-	struct work_struct rls_work;
 
 	/* HBA Errors */
 	u32 errors;
@@ -952,11 +951,10 @@ struct ufs_hba {
 
 	bool full_init_linereset;
 	struct pinctrl *pctrl;
-
+	
 	int			latency_hist_enabled;
 	struct io_latency_state io_lat_read;
 	struct io_latency_state io_lat_write;
-	bool restore_needed;
 };
 
 static inline void ufshcd_mark_shutdown_ongoing(struct ufs_hba *hba)
