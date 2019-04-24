16b3db431e38d0219534b1dd01f5d7b62d9a8738
Harshdeep Dhatt <hdhatt@codeaurora.org>
2019-04-24 12:13:43 -0600

    msm: kgsl: Omit referencing pointers in traces
    Trace points should not store pointers in the trace data because
    they could be dereferenced much later after the original memory
    has disappeared.
    
    Change-Id: I9f134bcdaa39c437394c3552aaacd8a360924200
    Signed-off-by: Harshdeep Dhatt <hdhatt@codeaurora.org>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- drivers/gpu/msm/adreno_trace.h
+++ drivers/gpu/msm/adreno_trace.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2017, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -600,15 +600,15 @@ TRACE_EVENT(adreno_preempt_trigger,
 	TP_PROTO(struct adreno_ringbuffer *cur, struct adreno_ringbuffer *next),
 	TP_ARGS(cur, next),
 	TP_STRUCT__entry(
-		__field(struct adreno_ringbuffer *, cur)
-		__field(struct adreno_ringbuffer *, next)
+		__field(unsigned int, cur)
+		__field(unsigned int, next)
 	),
 	TP_fast_assign(
-		__entry->cur = cur;
-		__entry->next = next;
+		__entry->cur = cur->id;
+		__entry->next = next->id;
 	),
 	TP_printk("trigger from id=%d to id=%d",
-		__entry->cur->id, __entry->next->id
+		__entry->cur, __entry->next
 	)
 );
 
@@ -616,15 +616,15 @@ TRACE_EVENT(adreno_preempt_done,
 	TP_PROTO(struct adreno_ringbuffer *cur, struct adreno_ringbuffer *next),
 	TP_ARGS(cur, next),
 	TP_STRUCT__entry(
-		__field(struct adreno_ringbuffer *, cur)
-		__field(struct adreno_ringbuffer *, next)
+		__field(unsigned int, cur)
+		__field(unsigned int, next)
 	),
 	TP_fast_assign(
-		__entry->cur = cur;
-		__entry->next = next;
+		__entry->cur = cur->id;
+		__entry->next = next->id;
 	),
 	TP_printk("done switch to id=%d from id=%d",
-		__entry->next->id, __entry->cur->id
+		__entry->next, __entry->cur
 	)
 );
 #endif /* _ADRENO_TRACE_H */
