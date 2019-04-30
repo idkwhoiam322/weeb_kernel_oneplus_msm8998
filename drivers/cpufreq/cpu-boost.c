643061e54b783cfa1aa2fdafb6798acd7babe02a
idkwhoiam322 <snkm1999@gmail.com>
2019-04-30 11:48:49 +0300

    Revert "cpu-boost: Update functions for newer Dynamic Schedtune Boost changes"
    This reverts commit f4e8d798fa1ef79163d094ec7a80eb3f6647831b.

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -44,7 +44,6 @@ module_param(input_boost_ms, uint, 0644);
 static int dynamic_stune_boost;
 module_param(dynamic_stune_boost, uint, 0644);
 static bool stune_boost_active;
-static int boost_slot;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 static struct delayed_work input_boost_rem;
@@ -179,7 +178,7 @@ static void do_input_boost_rem(struct work_struct *work)
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Reset dynamic stune boost value to the default value */
 	if (stune_boost_active) {
-		reset_stune_boost("top-app", boost_slot);
+		reset_stune_boost("top-app");
 		stune_boost_active = false;
 	}
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
@@ -196,7 +195,7 @@ static void do_input_boost(struct work_struct *work)
 	cancel_delayed_work_sync(&input_boost_rem);
 
 	if (stune_boost_active) {
-		reset_stune_boost("top-app", boost_slot);
+		reset_stune_boost("top-app");
 		stune_boost_active = false;
 	}
 
@@ -212,7 +211,7 @@ static void do_input_boost(struct work_struct *work)
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Set dynamic stune boost value */
-	ret = do_stune_boost("top-app", dynamic_stune_boost, &boost_slot);
+	ret = do_stune_boost("top-app", dynamic_stune_boost);
 	if (!ret)
 		stune_boost_active = true;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
@@ -274,7 +273,7 @@ static void cpuboost_input_disconnect(struct input_handle *handle)
 {
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Reset dynamic stune boost value to the default value */
-	reset_stune_boost("top-app", boost_slot);
+	reset_stune_boost("top-app");
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	input_close_device(handle);
