c6acc9a85cbc7731ddf8cb78c7407bc3cf18ad40
idkwhoiam322 <snkm1999@gmail.com>
2019-04-30 12:31:59 +0300

    drivers: staging: lowmemorykiller: define default minfree

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -53,14 +53,15 @@ static short lowmem_adj[6] = {
 	6,
 	12,
 };
-static int lowmem_adj_size = 4;
+static int lowmem_adj_size = 5;
 static int lowmem_minfree[6] = {
-	3 * 512,	/* 6MB */
-	2 * 1024,	/* 8MB */
-	4 * 1024,	/* 16MB */
-	16 * 1024,	/* 64MB */
+	18 * 1024,	/* 72MB */
+	22.5 * 1024,	/* 100MB */
+	31.5 * 1024,	/* 126MB */
+	54 * 1024,	/* 216MB */
+	78.75 * 1025,	/* 315MB */
 };
-static int lowmem_minfree_size = 4;
+static int lowmem_minfree_size = 5;
 
 static unsigned long lowmem_deathpending_timeout;
 
