87bfffea00d8025fd983c93965461f0159e3d05f
idkwhoiam322 <snkm1999@gmail.com>
2019-04-30 11:49:26 +0300

    Revert "sched/tune: Reset Dynamic Schedtune Boost only if no more boosts running"
    This reverts commit 36027d804487d54f2d3c6b8d658b6a11a2cfb1f3.

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -24,7 +24,6 @@ struct target_nrg schedtune_target_nrg;
 static DEFINE_MUTEX(stune_boost_mutex);
 static struct schedtune *getSchedtune(char *st_name);
 static int dynamic_boost(struct schedtune *st, int boost);
-static int stune_boost_count = 0;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 /* Performance Boost region (B) threshold params */
@@ -914,7 +913,6 @@ static int _do_stune_boost(struct schedtune *st, int boost)
 	int ret = 0;
 
 	mutex_lock(&stune_boost_mutex);
-	++stune_boost_count;
 
 	/* Boost if new value is greater than current */
 	if (boost > st->boost)
@@ -934,11 +932,7 @@ int reset_stune_boost(char *st_name)
 		return -EINVAL;
 
 	mutex_lock(&stune_boost_mutex);
-	if (stune_boost_count == 1)
-		ret = dynamic_boost(st, st->boost_default);
-
-	if (stune_boost_count >= 1)
-		--stune_boost_count;
+	ret = dynamic_boost(st, st->boost_default);
 	mutex_unlock(&stune_boost_mutex);
 
 	return ret;
