e2331d2c00f5503e2ad3078fb58d3eec5ceeff8a
idkwhoiam322 <snkm1999@gmail.com>
2019-05-03 22:26:22 +0300

    sched/features: disable min capacity capping

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -91,7 +91,7 @@ SCHED_FEAT(STUNE_BOOST_BIAS_BIG, false)
  * If enabled, this can be used to inform the scheduler about capacity
  * restrictions.
  */
-SCHED_FEAT(MIN_CAPACITY_CAPPING, true)
+SCHED_FEAT(MIN_CAPACITY_CAPPING, false)
 
 /*
  * Enforce the priority of candidates selected by find_best_target()
