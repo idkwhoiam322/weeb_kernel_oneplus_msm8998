de8a4dce308ea7247a1334ebe641c13499c3b3d1
idkwhoiam322 <snkm1999@gmail.com>
2019-05-03 22:17:16 +0300

    Revert "ANDROID: sched/fair: prefer energy efficient CPUs for !prefer_idle tasks"
    This reverts commit 716c0679b8efae9cd65dc133129c674fd3d1b7d6.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6804,8 +6804,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				 * IOW, prefer a deep IDLE LITTLE CPU vs a
 				 * shallow idle big CPU.
 				 */
-				if (capacity_orig == target_capacity &&
-				    sysctl_sched_cstate_aware &&
+				if (sysctl_sched_cstate_aware &&
 				    best_idle_cstate <= idle_idx)
 					continue;
 
@@ -6849,8 +6848,8 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				continue;
 
 			/* Favor CPUs with maximum spare capacity */
-			if (capacity_orig == target_capacity &&
-			   (capacity_orig - min_capped_util) < target_max_spare_cap)
+			if ((capacity_orig - min_capped_util) <
+				target_max_spare_cap)
 				continue;
 
 			target_max_spare_cap = capacity_orig - min_capped_util;
