71ac360ca8872f32e18edc0193bdac9e02ef25e8
Sultan Alsawaf <sultan@kerneltoast.com>
2019-05-05 14:54:15 -0700

    qcacld-3.0: Nuke as much debug bloat as possible
    The overhead from all the debugging in this monstrosity of a driver is
    measurably significant. Chop it all out.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    idkwhoiam322: adjust for caf qcacld
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_power.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_power.c
@@ -1482,7 +1482,6 @@ QDF_STATUS hdd_wlan_shutdown(void)
 	cds_clear_concurrent_session_count();
 
 	hdd_debug("Invoking packetdump deregistration API");
-	wlan_deregister_txrx_packetdump();
 
 	/*
 	 * After SSR, FW clear its txrx stats. In host,
