c3eaf6f7f057d23402b7e5da471d69fb1174c737
idkwhoiam322 <snkm1999@gmail.com>
2019-05-12 00:06:00 +0300

    Revert "qcacld-3.0: Add diag events for debugging"
    This reverts commit 06ffdaf27c270fc3f3fc787cb010e64661bdb3b6.
    Signed-off-by: idkwhoiam322 <snkm1999@gmail.com>

--- drivers/staging/qcacld-3.0/core/utils/host_diag_log/inc/host_diag_core_event.h
+++ drivers/staging/qcacld-3.0/core/utils/host_diag_log/inc/host_diag_core_event.h
@@ -45,7 +45,6 @@ extern "C" {
 #endif /* __cplusplus */
 
 #define WAKE_LOCK_NAME_LEN 80
-#define RSN_OUI_SIZE 4
 
 /*-------------------------------------------------------------------------
    Event ID: EVENT_WLAN_SECURITY
@@ -306,27 +305,6 @@ struct host_event_wlan_low_resource_failure {
  */
 enum resource_failure_type {
 	WIFI_EVENT_MEMORY_FAILURE,
-};
-
-/*-------------------------------------------------------------------------
-  Event ID: EVENT_WLAN_RSN_INFO
-  -------------------------------------------------------------------------
- */
-/**
- * struct event_wlan_csr_rsn_info - Structure holding the
- * RSN information for assoc request
- * @akm_suite: Gives information about akm suites used in assoc request
- * @ucast_cipher: Unicast cipher used in assoc request
- * @mcast_cipher: Multicast cipher used in assoc request
- * @group_mgmt: Requested group mgmt cipher suite
- *
- * This structure will hold the RSN information for assoc request
- */
-struct event_wlan_csr_rsn_info {
-	uint8_t   akm_suite[RSN_OUI_SIZE];
-	uint8_t   ucast_cipher[RSN_OUI_SIZE];
-	uint8_t   mcast_cipher[RSN_OUI_SIZE];
-	uint8_t   group_mgmt[RSN_OUI_SIZE];
 };
 
 /*-------------------------------------------------------------------------
