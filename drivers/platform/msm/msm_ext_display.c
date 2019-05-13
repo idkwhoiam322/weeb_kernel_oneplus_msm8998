29e3d45b8d3377ff76eabdf961ec4b6c58680411
idkwhoiam322 <snkm1999@gmail.com>
2019-05-13 18:17:23 +0300

    Revert "drivers: video: mdss: Fix direct references to HZ"
    This reverts commit b39f1fa21f5415e7195aa26a7c24a329c67916b2.

--- drivers/platform/msm/msm_ext_display.c
+++ drivers/platform/msm/msm_ext_display.c
@@ -238,8 +238,7 @@ static int msm_ext_disp_process_display(struct msm_ext_disp *ext_disp,
 	}
 
 	reinit_completion(&ext_disp->hpd_comp);
-	ret = wait_for_completion_timeout(&ext_disp->hpd_comp,
-			msecs_to_jiffies(5000));
+	ret = wait_for_completion_timeout(&ext_disp->hpd_comp, HZ * 5);
 	if (!ret) {
 		pr_err("display timeout\n");
 		ret = -EINVAL;
@@ -273,8 +272,7 @@ static int msm_ext_disp_process_audio(struct msm_ext_disp *ext_disp,
 	}
 
 	reinit_completion(&ext_disp->hpd_comp);
-	ret = wait_for_completion_timeout(&ext_disp->hpd_comp,
-			msecs_to_jiffies(2000));
+	ret = wait_for_completion_timeout(&ext_disp->hpd_comp, HZ * 2);
 	if (!ret) {
 		pr_err("audio timeout\n");
 		ret = -EINVAL;
