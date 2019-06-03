af47727ed71d29365815c98ab2065b01b6faaafd
Rick Yiu <rickyiu@google.com>
2019-06-03 19:17:17 +0800

    sched/fair: let scheduler skip util checking if cpu is idle
    Current cpu util includes util of runnable tasks plus the recent utilization
    of currently non-runnable tasks, so it may return a non-zero value even
    there is no task running on a cpu. When scheduler is selecting a cpu for
    a task, it will check if cpu util is over its capacity, what could
    happen is that it will skip a cpu even it is idle, so let scheduler
    skip util checking if the task perfers idle cpu and the cpu is idle.
    
    Bug: 133284637
    Test: cpu selected as expected
    Change-Id: I2c15d6b79b1cc83c72e84add70962a8e74c178b8
    Signed-off-by: Rick Yiu <rickyiu@google.com>
    idkwhoiam322: Port to 4.4
    Cherry-picked from: https://android.googlesource.com/kernel/msm.git/+/19a38e4ecd42df84f47e56b492ebed704e9e4058
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5096,6 +5096,8 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 #ifdef CONFIG_SMP
 	int task_new = flags & ENQUEUE_WAKEUP_NEW;
 #endif
+	bool prefer_idle = sched_feat(EAS_PREFER_IDLE) ?
+				(schedtune_prefer_idle(p) > 0) : 0;
 
 	/*
 	 * The code below (indirectly) updates schedutil which looks at
@@ -5166,7 +5168,14 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	if (!se) {
 		add_nr_running(rq, 1);
 		walt_inc_cumulative_runnable_avg(rq, p);
-		if (!task_new)
+		/*
+		 * If the task prefers idle cpu, and it also is the first
+		 * task enqueued in this runqueue, then we don't check
+		 * overutilized. Hopefully the cpu util will be back to
+		 * normal before next overutilized check.
+		 */
+		if (!task_new &&
+			!(prefer_idle && rq->nr_running == 1))
 			update_overutilized_status(rq);
 	}
 
@@ -7114,6 +7123,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 	unsigned long min_wake_util = ULONG_MAX;
 	unsigned long target_max_spare_cap = 0;
 	unsigned long best_active_util = ULONG_MAX;
+	unsigned long best_idle_util = ULONG_MAX;
 	unsigned long target_idle_max_spare_cap = 0;
 	int best_idle_cstate = INT_MAX;
 	struct sched_domain *sd;
@@ -7185,6 +7195,8 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 * Ensure minimum capacity to grant the required boost.
 			 * The target CPU can be already at a capacity level higher
 			 * than the one required to boost the task.
+			 * However, if the task prefers idle cpu and that
+			 * cpu is idle, skip this check.
 			 */
 			new_util = max(min_util, new_util);
 
@@ -7197,7 +7209,8 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 */
 			min_capped_util = max(new_util, capacity_min_of(i));
 
-			if (new_util > capacity_orig)
+			if (!(prefer_idle && idle_cpu(i))
+				&& new_util > capacity_orig)
 				continue;
 
 			if (idle_cpu(i))
@@ -7260,12 +7273,22 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 					    capacity_orig > target_capacity)
 						continue;
 					if (capacity_orig == target_capacity &&
-					    sysctl_sched_cstate_aware &&
-					    best_idle_cstate <= idle_idx)
-						continue;
+					    sysctl_sched_cstate_aware) {
+						if (best_idle_cstate < idle_idx)
+							continue;
+						/*
+						 * If idle state of cpu is the
+						 * same, select least utilized.
+						 */
+						else if (best_idle_cstate ==
+						    idle_idx &&
+						    best_idle_util <= new_util)
+							continue;
+					}
 
 					target_capacity = capacity_orig;
 					best_idle_cstate = idle_idx;
+					best_idle_util = new_util;
 					best_idle_cpu = i;
 					continue;
 				}
