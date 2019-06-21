49e5c088fafb462fcb600a07b4abfc4b2718d7b9
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2019-06-21 16:36:43 +0200

    posix-timers: Use spin_lock_irq() in itimer_delete()
    itimer_delete() uses spin_lock_irqsave() to obtain a `flags' variable
    which can then be passed to unlock_timer(). It uses already spin_lock
    locking for the structure instead of lock_timer() because it has a timer
    which can not be removed by others at this point. The cleanup is always
    performed with enabled interrupts.
    
    Use spin_lock_irq() / spin_unlock_irq() so the `flags' variable can be
    removed.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Link: https://lkml.kernel.org/r/20190621143643.25649-3-bigeasy@linutronix.de
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- kernel/time/posix-timers.c
+++ kernel/time/posix-timers.c
@@ -988,13 +988,11 @@ retry_delete:
  */
 static void itimer_delete(struct k_itimer *timer)
 {
-	unsigned long flags;
-
 retry_delete:
-	spin_lock_irqsave(&timer->it_lock, flags);
+	spin_lock_irq(&timer->it_lock);
 
 	if (timer_delete_hook(timer) == TIMER_RETRY) {
-		unlock_timer(timer, flags);
+		spin_unlock_irq(&timer->it_lock);
 		goto retry_delete;
 	}
 	list_del(&timer->list);
@@ -1004,7 +1002,7 @@ retry_delete:
 	 */
 	timer->it_signal = NULL;
 
-	unlock_timer(timer, flags);
+	spin_unlock_irq(&timer->it_lock);
 	release_posix_timer(timer, IT_ID_SET);
 }
 
