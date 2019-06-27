dcd13085209a5cfca39df56c26f6062eb834a768
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-06-28 00:06:45 +0530

    Revert "sched/core: Use the new llist_for_each_entry_safe() primitive"
    This reverts commit b67a6cbd93aa4360b32234a019cd18b379b058e3.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1925,7 +1925,7 @@ void sched_ttwu_pending(void)
 {
 	struct rq *rq = this_rq();
 	struct llist_node *llist = llist_del_all(&rq->wake_list);
-        struct task_struct *p, *t;
+	struct task_struct *p;
 	unsigned long flags;
 
 	if (!llist)
@@ -1934,8 +1934,17 @@ void sched_ttwu_pending(void)
 	raw_spin_lock_irqsave(&rq->lock, flags);
 	lockdep_pin_lock(&rq->lock);
 
-	llist_for_each_entry_safe(p, t, llist, wake_entry)
-		ttwu_do_activate(rq, p, p->sched_remote_wakeup ? WF_MIGRATED : 0);
+	while (llist) {
+		int wake_flags = 0;
+
+		p = llist_entry(llist, struct task_struct, wake_entry);
+		llist = llist_next(llist);
+
+		if (p->sched_remote_wakeup)
+			wake_flags = WF_MIGRATED;
+
+		ttwu_do_activate(rq, p, wake_flags);
+	}
 
 	lockdep_unpin_lock(&rq->lock);
 	raw_spin_unlock_irqrestore(&rq->lock, flags);
