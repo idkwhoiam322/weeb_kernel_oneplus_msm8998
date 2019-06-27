1fff0ce59d23671153c8553252dd5e0cbcc48e56
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-06-28 00:06:47 +0530

    Revert "sched/fair: Prepare to fix fairness problems on migration"
    This reverts commit aafa960669eeebb68184fca9ca1e02063d4a111d.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1878,22 +1878,14 @@ ttwu_do_wakeup(struct rq *rq, struct task_struct *p, int wake_flags)
 static void
 ttwu_do_activate(struct rq *rq, struct task_struct *p, int wake_flags)
 {
-	int en_flags = ENQUEUE_WAKEUP;
-
 	lockdep_assert_held(&rq->lock);
 
 #ifdef CONFIG_SMP
 	if (p->sched_contributes_to_load)
 		rq->nr_uninterruptible--;
-
-	/*
-	 * If we migrated; we must have called sched_class::task_waking().
-	 */
-	if (wake_flags & WF_MIGRATED)
-		en_flags |= ENQUEUE_WAKING;
 #endif
 
-	ttwu_activate(rq, p, en_flags);
+	ttwu_activate(rq, p, ENQUEUE_WAKEUP | ENQUEUE_WAKING);
 	ttwu_do_wakeup(rq, p, wake_flags);
 }
 
@@ -1937,11 +1929,7 @@ void sched_ttwu_pending(void)
 	while (llist) {
 		p = llist_entry(llist, struct task_struct, wake_entry);
 		llist = llist_next(llist);
-		/*
-		 * See ttwu_queue(); we only call ttwu_queue_remote() when
-		 * its a x-cpu wakeup.
-		 */
-		ttwu_do_activate(rq, p, WF_MIGRATED);
+		ttwu_do_activate(rq, p, 0);
 	}
 
 	lockdep_unpin_lock(&rq->lock);
@@ -2028,7 +2016,7 @@ bool cpus_share_cache(int this_cpu, int that_cpu)
 }
 #endif /* CONFIG_SMP */
 
-static void ttwu_queue(struct task_struct *p, int cpu, int wake_flags)
+static void ttwu_queue(struct task_struct *p, int cpu)
 {
 	struct rq *rq = cpu_rq(cpu);
 
@@ -2042,7 +2030,7 @@ static void ttwu_queue(struct task_struct *p, int cpu, int wake_flags)
 
 	raw_spin_lock(&rq->lock);
 	lockdep_pin_lock(&rq->lock);
-        ttwu_do_activate(rq, p, wake_flags);
+	ttwu_do_activate(rq, p, 0);
 	lockdep_unpin_lock(&rq->lock);
 	raw_spin_unlock(&rq->lock);
 }
@@ -2165,6 +2153,9 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags,
 	p->sched_contributes_to_load = !!task_contributes_to_load(p);
 	p->state = TASK_WAKING;
 
+	if (p->sched_class->task_waking)
+		p->sched_class->task_waking(p);
+
 	cpu = select_task_rq(p, p->wake_cpu, SD_BALANCE_WAKE, wake_flags,
 			     sibling_count_hint);
 
@@ -2172,16 +2163,12 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags,
 	src_cpu = task_cpu(p);
 	if (src_cpu != cpu) {
 		wake_flags |= WF_MIGRATED;
-
-		if (p->sched_class->task_waking)
-			p->sched_class->task_waking(p);
-
 		set_task_cpu(p, cpu);
 	}
 
 #endif /* CONFIG_SMP */
 
-	ttwu_queue(p, cpu, wake_flags);
+	ttwu_queue(p, cpu);
 stat:
 	if (schedstat_enabled())
 		ttwu_stat(p, cpu, wake_flags);
