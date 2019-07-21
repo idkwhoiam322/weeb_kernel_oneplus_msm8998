797374913200f6bac638e1c566e8c4bb340f9226
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-07-21 18:36:30 +0530

    input: touch: synaptics_driver_s3320: Optimize interrupt CPU usage
    Ported from https://github.com/kerneltoast/android_kernel_google_wahoo/commit/dd0b9662dbe9d23465653d724fba0523b5c601a1
    Don't set irq as perf critical.
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- drivers/input/touchscreen/synaptics_driver_s3320.c
+++ drivers/input/touchscreen/synaptics_driver_s3320.c
@@ -30,6 +30,7 @@
 #include <linux/interrupt.h>
 #include <linux/regulator/consumer.h>
 #include <linux/firmware.h>
+#include <linux/i2c/i2c-msm-v2.h>
 
 #include <linux/pinctrl/consumer.h>
 #include <linux/pinctrl/pinctrl.h>
@@ -511,7 +512,8 @@ struct synaptics_ts_data {
 	struct kobject *properties_kobj;
 #endif
 
-	struct pm_qos_request pm_qos_req;
+	struct pm_qos_request pm_i2c_req;
+	struct pm_qos_request pm_touch_req;
 };
 
 static struct device_attribute attrs_oem[] = {
@@ -1640,7 +1642,8 @@ static irqreturn_t synaptics_irq_thread_fn(int irq, void *dev_id)
 
 
 	/* prevent CPU from entering deep sleep */
-	pm_qos_update_request(&ts->pm_qos_req, 100);
+	pm_qos_update_request(&ts->pm_touch_req, 100);
+	pm_qos_update_request(&ts->pm_i2c_req, 100);
 
 	synaptics_rmi4_i2c_write_byte(ts->client, 0xff, 0x00);
 	ret = synaptics_rmi4_i2c_read_word(ts->client, F01_RMI_DATA_BASE);
@@ -1683,7 +1686,8 @@ static irqreturn_t synaptics_irq_thread_fn(int irq, void *dev_id)
 
 
 END:
-	pm_qos_update_request(&ts->pm_qos_req, PM_QOS_DEFAULT_VALUE);
+	pm_qos_update_request(&ts->pm_i2c_req, PM_QOS_DEFAULT_VALUE);
+	pm_qos_update_request(&ts->pm_touch_req, PM_QOS_DEFAULT_VALUE);
 
 	return IRQ_HANDLED;
 }
@@ -4559,6 +4563,7 @@ static int synaptics_ts_probe(struct i2c_client *client,
 	uint8_t buf[4];
 	uint32_t CURRENT_FIRMWARE_ID = 0;
 	uint32_t bootloader_mode;
+	struct i2c_msm_ctrl *ctrl;
 
 	TPD_ERR("%s  is called\n", __func__);
 
@@ -4568,9 +4573,17 @@ static int synaptics_ts_probe(struct i2c_client *client,
 		goto err_alloc_data_failed;
 	}
 
+	ctrl = client->dev.parent->driver_data;
 
-	pm_qos_add_request(&ts->pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
-		PM_QOS_DEFAULT_VALUE);
+	ts->pm_i2c_req.type = PM_QOS_REQ_AFFINE_IRQ;
+	ts->pm_i2c_req.irq = ctrl->rsrcs.irq;
+	pm_qos_add_request(&ts->pm_i2c_req, PM_QOS_CPU_DMA_LATENCY,
+			   PM_QOS_DEFAULT_VALUE);
+
+	ts->pm_touch_req.type = PM_QOS_REQ_AFFINE_IRQ;
+	ts->pm_touch_req.irq = ts->irq;
+	pm_qos_add_request(&ts->pm_touch_req, PM_QOS_CPU_DMA_LATENCY,
+			   PM_QOS_DEFAULT_VALUE);
 
 	ts->client = client;
 	i2c_set_clientdata(client, ts);
@@ -4800,7 +4813,6 @@ err_alloc_data_failed:
 	kfree(ts);
 	ts = NULL;
 	ts_g = NULL;
-	pm_qos_remove_request(&ts->pm_qos_req);
 	TPD_ERR("synaptics_ts_probe: not normal end\n");
 	return ret;
 }
@@ -4829,8 +4841,6 @@ static int synaptics_ts_remove(struct i2c_client *client)
 	input_free_device(ts->input_dev);
 	kfree(ts);
 	tpd_power(ts, 0);
-
-	pm_qos_remove_request(&ts->pm_qos_req);
 
 	return 0;
 }
