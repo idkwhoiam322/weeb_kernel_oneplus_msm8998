68095d72f281f9aa6c170c67cab689f5d30babdd
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-07-27 22:11:48 +0530

    Revert "ANDROID: sched/fair: add idle state filter to prefer_idle case"
    This reverts commit 19269763f5f739a144f4ec61cccf225474dce360.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6961,7 +6961,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			unsigned long capacity_orig = capacity_orig_of(i);
 			unsigned long wake_util, new_util;
 			long spare_cap;
-			int idle_idx = INT_MAX;
 
 			if (!cpu_online(i) || cpu_isolated(i))
 				continue;
@@ -6992,10 +6991,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 * enqueued here.
 			 */
 			spare_cap = capacity_orig - new_util;
-
-			if (idle_cpu(i))
-				idle_idx = idle_get_state_idx(cpu_rq(i));
-
 
 			/*
 			 * Case A) Latency sensitive tasks
@@ -7040,18 +7035,13 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 					schedstat_inc(this_rq(), eas_stats.fbt_pref_idle);
 
 					if (boosted &&
-					    capacity_orig < target_capacity)
+					    capacity_orig <= target_capacity)
 						continue;
 					if (!boosted &&
-					    capacity_orig > target_capacity)
-						continue;
-					if (capacity_orig == target_capacity &&
-					    sysctl_sched_cstate_aware &&
-					    best_idle_cstate <= idle_idx)
+					    capacity_orig >= target_capacity)
 						continue;
 
 					target_capacity = capacity_orig;
-					best_idle_cstate = idle_idx;
 					best_idle_cpu = i;
 					continue;
 				}
@@ -7133,6 +7123,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 * consumptions without affecting performance.
 			 */
 			if (idle_cpu(i)) {
+				int idle_idx = idle_get_state_idx(cpu_rq(i));
 
 				/* Select idle CPU with lower cap_orig */
 				if (capacity_orig > best_idle_min_cap_orig)
