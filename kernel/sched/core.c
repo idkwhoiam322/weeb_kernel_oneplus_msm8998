072cbbac888a3abf641c09bd00e25db8f3528e6a
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-07-27 22:03:22 +0530

    Revert "sched/core: Get rid of 'cpu' argument in wq_worker_sleeping()"
    This reverts commit 8d3937bf98d9566bfafd5f4293bbefccd365b184.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3437,7 +3437,7 @@ static void __sched notrace __schedule(bool preempt)
 			if (prev->flags & PF_WQ_WORKER) {
 				struct task_struct *to_wakeup;
 
-				to_wakeup = wq_worker_sleeping(prev);
+				to_wakeup = wq_worker_sleeping(prev, cpu);
 				if (to_wakeup)
 					try_to_wake_up_local(to_wakeup);
 			}
