1d9c71bd49e7ba81c71c4d4fbcdfef0762943151
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-07-27 22:14:26 +0530

    Revert "sched/fair: Remove 'cpu_busy' parameter from update_next_balance()"
    This reverts commit 8a502b038cc258f74be922a0b37904dc22104628.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -10019,12 +10019,11 @@ get_sd_balance_interval(struct sched_domain *sd, int cpu_busy)
 }
 
 static inline void
-update_next_balance(struct sched_domain *sd, unsigned long *next_balance)
+update_next_balance(struct sched_domain *sd, int cpu_busy, unsigned long *next_balance)
 {
 	unsigned long interval, next;
 
-	/* used by idle balance, so cpu_busy = 0 */
-	interval = get_sd_balance_interval(sd, 0);
+	interval = get_sd_balance_interval(sd, cpu_busy);
 	next = sd->last_balance + interval;
 
 	if (time_after(*next_balance, next))
@@ -10063,7 +10062,7 @@ static int idle_balance(struct rq *this_rq)
 		rcu_read_lock();
 		sd = rcu_dereference_check_sched_domain(this_rq->sd);
 		if (sd)
-			update_next_balance(sd, &next_balance);
+			update_next_balance(sd, 0, &next_balance);
 		rcu_read_unlock();
 
 		goto out;
@@ -10085,7 +10084,7 @@ static int idle_balance(struct rq *this_rq)
 		}
 
 		if (this_rq->avg_idle < curr_cost + sd->max_newidle_lb_cost) {
-			update_next_balance(sd, &next_balance);
+			update_next_balance(sd, 0, &next_balance);
 			break;
 		}
 
@@ -10103,7 +10102,7 @@ static int idle_balance(struct rq *this_rq)
 			curr_cost += domain_cost;
 		}
 
-		update_next_balance(sd, &next_balance);
+		update_next_balance(sd, 0, &next_balance);
 
 		/*
 		 * Stop searching for tasks to pull if there are
