6e71a9727be64955396e02cb56a4ae2ccd06abb5
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-08-07 22:28:19 +0530

    Revert "sched/swait: Document it clearly that the swait facilities are special and shouldn't be used"
    This reverts commit d59ddf796c9f1f2bd8bce6bc771cf21559649fc3.

--- include/linux/swait.h
+++ include/linux/swait.h
@@ -22,16 +22,13 @@
  * since there is no "queue". Use "wake_up_process()" with a direct
  * pointer instead.
  *
- * While these are very similar to regular wait queues (wait.h) the most
- * important difference is that the simple waitqueue allows for deterministic
- * behaviour -- IOW it has strictly bounded IRQ and lock hold times.
+ * While these are very similar to the other/complex wait queues (wait.h) the
+ * most important difference is that the simple waitqueue allows for
+ * deterministic behaviour -- IOW it has strictly bounded IRQ and lock hold
+ * times.
  *
- * Mainly, this is accomplished by two things. Firstly not allowing swake_up_all
- * from IRQ disabled, and dropping the lock upon every wakeup, giving a higher
- * priority task a chance to run.
- *
- * Secondly, we had to drop a fair number of features of the other waitqueue
- * code; notably:
+ * In order to make this so, we had to drop a fair number of features of the
+ * other waitqueue code; notably:
  *
  *  - mixing INTERRUPTIBLE and UNINTERRUPTIBLE sleeps on the same waitqueue;
  *    all wakeups are TASK_NORMAL in order to avoid O(n) lookups for the right
@@ -40,14 +37,12 @@
  *  - the !exclusive mode; because that leads to O(n) wakeups, everything is
  *    exclusive.
  *
- *  - custom wake callback functions; because you cannot give any guarantees
- *    about random code. This also allows swait to be used in RT, such that
- *    raw spinlock can be used for the swait queue head.
+ *  - custom wake functions; because you cannot give any guarantees about
+ *    random code.
  *
- * As a side effect of these; the data structures are slimmer albeit more ad-hoc.
- * For all the above, note that simple wait queues should _only_ be used under
- * very specific realtime constraints -- it is best to stick with the regular
- * wait queues in most cases.
+ * As a side effect of this; the data structures are slimmer.
+ *
+ * One would recommend using this wait queue where possible.
  */
 
 struct task_struct;
