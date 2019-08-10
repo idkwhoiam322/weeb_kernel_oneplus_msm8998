65811a54e663dd4f72aa449696f71c3f2912248d
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-08-10 11:25:56 +0530

    sched: features: Disable SCHEDTUNE_BOOST_UTIL feature
    The primary reason for for using a schedtune.boost of 1 for the top-app is the
    biasing stune does to big cluster. However, it also boosts the utilisation by
    the same factor, that is, 1%. Since that is not what I want,
    disable SCHEDTUNE_BOOST_UTIL and observe for regressions.
    
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -116,4 +116,4 @@ SCHED_FEAT(MIN_CAPACITY_CAPPING, false)
  * generally leads to usage of higher frequencies.
  * If disabled, boosts will only bias tasks to higher-capacity CPUs.
  */
-SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, true)
+SCHED_FEAT(SCHEDTUNE_BOOST_UTIL, false)
