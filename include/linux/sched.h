39775c82ef19c5a5b4934ef5b56e7c9af609d646
celtare21 <celtare21@gmail.com>
2019-08-18 08:19:02 +0000

    Revert "softirq: defer softirq processing to ksoftirqd if CPU is busy with RT"
    This reverts commit a66b3eb5aff1536bc32e5e5eb60393610a21dd06.
    
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2518,7 +2518,6 @@ extern void do_set_cpus_allowed(struct task_struct *p,
 
 extern int set_cpus_allowed_ptr(struct task_struct *p,
 				const struct cpumask *new_mask);
-extern bool cpupri_check_rt(void);
 static inline void set_wake_up_idle(bool enabled)
 {
 	/* do nothing for now */
@@ -2534,10 +2533,6 @@ static inline int set_cpus_allowed_ptr(struct task_struct *p,
 	if (!cpumask_test_cpu(0, new_mask))
 		return -EINVAL;
 	return 0;
-}
-static inline bool cpupri_check_rt(void)
-{
-	return false;
 }
 #endif
 
