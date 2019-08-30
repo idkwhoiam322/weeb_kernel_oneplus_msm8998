ceba0dbe7cae23980d0eff77770ea503d8eaa2ee
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-08-30 15:14:05 +0530

    drivers: irqchip: Remove OP changes
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -37,7 +37,6 @@
 #include <asm/virt.h>
 
 #include "irq-gic-common.h"
-#include <linux/wakeup_reason.h>
 
 struct redist_region {
 	void __iomem		*redist_base;
@@ -442,7 +441,7 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 			name = "stray irq";
 		else if (desc->action && desc->action->name)
 			name = desc->action->name;
-		log_wakeup_reason(irq);
+
 		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
 	}
 }
