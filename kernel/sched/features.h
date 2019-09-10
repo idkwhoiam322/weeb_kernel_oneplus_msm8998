f87ab38313cae64f10164e06a9a6f8522d3d6105
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-09-10 17:47:20 +0530

    Revert "sched/features: disable min capacity capping"
    This reverts commit e2331d2c00f5503e2ad3078fb58d3eec5ceeff8a.

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -109,7 +109,7 @@ SCHED_FEAT(STUNE_BOOST_BIAS_BIG, true)
  * If enabled, this can be used to inform the scheduler about capacity
  * restrictions.
  */
-SCHED_FEAT(MIN_CAPACITY_CAPPING, false)
+SCHED_FEAT(MIN_CAPACITY_CAPPING, true)
 
 /*
  * Inflate the effective utilization of SchedTune-boosted tasks, which
