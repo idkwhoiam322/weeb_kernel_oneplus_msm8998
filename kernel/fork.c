8cee0156cc867042ef5a1af103a849532de9b595
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-09-20 22:37:28 +0530

    Revert "kernel: Boost to the max for a short amount of time when zygote forks"
    This reverts commit 204453b3773f1ffe687146dcbe5d0c790208d1af.

--- kernel/fork.c
+++ kernel/fork.c
@@ -79,7 +79,6 @@
 #include <linux/sysctl.h>
 #include <linux/kcov.h>
 #include <linux/cpufreq_times.h>
-#include <linux/devfreq_boost.h>
 #include <linux/simple_lmk.h>
 
 #include <asm/pgtable.h>
@@ -1794,11 +1793,6 @@ long _do_fork(unsigned long clone_flags,
 	struct task_struct *p;
 	int trace = 0;
 	long nr;
-
-	/* Boost CPU to the max for 50 ms when userspace launches an app */
-	if (task_is_zygote(current)) {
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
-	}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
