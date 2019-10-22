92a249f8b35707a574fec993313c0d93fdb4710a
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-10-23 00:13:57 +0530

    schedutil: re-enable iowait boost
    Signed-off-by: idkwhoiam322 <idkwhoiam322@raphielgang.org>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -816,7 +816,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	}
 
 	if (task_is_booster(current))
-		tunables->iowait_boost_enable = false;
+		tunables->iowait_boost_enable = true;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
