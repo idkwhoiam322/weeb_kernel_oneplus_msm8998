4c560d5a5b78d99b703700f87d3084cf8c1108dc
Sultan Alsawaf <sultan@kerneltoast.com>
2019-11-04 11:06:13 -0800

    simple_lmk: Make reclaim deterministic
    The 20 ms delay in the reclaim thread is a hacky fudge factor that can
    cause Simple LMK to behave wildly differently depending on the
    circumstances of when it is invoked. When kswapd doesn't get enough CPU
    time to finish up and go back to sleep within 20 ms, Simple LMK performs
    superfluous reclaims.
    
    This is suboptimal, so make Simple LMK more deterministic by eliminating
    the delay and instead queuing up reclaim requests from kswapd.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3471,7 +3471,6 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int order,
 	/* Try to sleep for a short interval */
 	if (prepare_kswapd_sleep(pgdat, order, remaining,
 						balanced_classzone_idx)) {
-		simple_lmk_stop_reclaim();
 		/*
 		 * Compaction records what page blocks it recently failed to
 		 * isolate pages from and skips them in the future scanning.
@@ -3497,7 +3496,6 @@ static void kswapd_try_to_sleep(pg_data_t *pgdat, int order,
 	 */
 	if (prepare_kswapd_sleep(pgdat, order, remaining,
 						balanced_classzone_idx)) {
-		simple_lmk_stop_reclaim();
 		trace_mm_vmscan_kswapd_sleep(pgdat->node_id);
 
 		/*
