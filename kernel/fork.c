480d1070008b6226888a7204a5c94983e6744537
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-11-25 17:33:14 +0530

    kernel: boost devfreq to max for cold app launches
    Additionally, generalize the comment preceding the max_boost block.

--- kernel/fork.c
+++ kernel/fork.c
@@ -81,6 +81,7 @@
 #include <linux/cpufreq_times.h>
 #include <linux/simple_lmk.h>
 #include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1799,9 +1800,11 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost CPU to the max for 2500 ms when userspace launches an app */
-	if (task_is_zygote(current))
+	/* Boost to max for 2500 ms when userspace launches an app */
+	if (task_is_zygote(current)) {
 		cpu_input_boost_kick_max(2500);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 2500);
+	}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
