2480fff2ae235d84b04c662c1dab3c5a3787adbf
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-12-03 12:45:58 +0300

    Revert "power: reset: msm: set restart reason to oem panic if in_panic or unspecified command"
    This reverts commit 412df2faa8d044e549d9a3c5a600886fe433179f.

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -312,11 +312,7 @@ static void msm_restart_prepare(const char *cmd)
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_HARD_RESET);
 	}
 
-	/* Set restart reason to oem panic if in panic or unspecified command */
-	if (in_panic || !cmd) {
-		qpnp_pon_set_restart_reason(PON_RESTART_REASON_PANIC);
-		__raw_writel(OEM_PANIC, restart_reason);
-	} else {
+	if (cmd != NULL) {
 		if (!strncmp(cmd, "bootloader", 10)) {
 			qpnp_pon_set_restart_reason(
 				PON_RESTART_REASON_BOOTLOADER);
