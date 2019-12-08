067924922c7cabacac4f6f9e7e5f5133033ff142
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2019-12-08 22:30:33 +0300

    Revert "schedutil: add task_is_booster() protection to schedutil tuneables"
    This reverts commit e76a3d3f4422041a51f581d4a708f6c72c48eed4.

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -753,12 +753,10 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	if (task_is_booster(current)){
-		tunables->up_rate_limit_us = 500;
-		tunables->down_rate_limit_us = 20000;
-		tunables->iowait_boost_enable = true;
-	}
+	tunables->up_rate_limit_us = 500;
+	tunables->down_rate_limit_us = 20000;
 
+	tunables->iowait_boost_enable = true;
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
 
