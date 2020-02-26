78fc4874abb98ed357de998ce2785439e4d821c4
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-02-27 00:13:09 +0530

    Revert "proc: Stop OnePlus Camera from running in the background"
    This reverts commit 08feb0312efeba435affe614cc0e9a2e97667f8b.

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1255,9 +1255,7 @@ out:
 	/* These apps burn through CPU in the background. Don't let them. */
 	if (!err && oom_score_adj >= 700) {
 		if (!strcmp(task_comm, "id.GoogleCamera") ||
-		    !strcmp(task_comm, "ndroid.settings") ||
-		    !strcmp(task_comm, "eaurora.snapcam") ||
-		    !strcmp(task_comm, ".oneplus.camera")) {
+		    !strcmp(task_comm, "eaurora.snapcam")) {
 			struct task_kill_info *kinfo;
 
 			kinfo = kmalloc(sizeof(*kinfo), GFP_KERNEL);
