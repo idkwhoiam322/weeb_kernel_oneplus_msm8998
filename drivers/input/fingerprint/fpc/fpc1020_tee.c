a0ec86ca0dcd027fe6f6865a066e08314dd87368
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-03-14 22:01:07 +0530

    Revert "treewide: remove perf critical patchset"
    This reverts commit 251dd759c25fbbdbd9ba9bb8d39c5e646a5e978f.

--- drivers/input/fingerprint/fpc/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc/fpc1020_tee.c
@@ -603,7 +603,7 @@ static int fpc1020_probe(struct platform_device *pdev)
     fpc1020->screen_state = 1;
     #endif
 
-	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT;
+	irqf = IRQF_TRIGGER_RISING | IRQF_ONESHOT | IRQF_PERF_CRITICAL;
 	mutex_init(&fpc1020->lock);
 	rc = devm_request_threaded_irq(dev, gpio_to_irq(fpc1020->irq_gpio),
 			NULL, fpc1020_irq_handler, irqf,
