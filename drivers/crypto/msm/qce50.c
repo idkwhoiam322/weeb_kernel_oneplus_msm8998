8ad8fdffad3f79df3216da9f69ebc8619e82d10d
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-04-09 16:09:36 +0530

    Revert "crypto: msm: Use for_each_sg() macro where possible"
    This reverts commit bc60af480a10d537442e6bce564668785f7f82d6.

--- drivers/crypto/msm/qce50.c
+++ drivers/crypto/msm/qce50.c
@@ -227,8 +227,10 @@ static int qce_dma_map_sg(struct device *dev, struct scatterlist *sg, int nents,
 {
 	int i;
 
-	for_each_sg(sg, sg, nents, i)
+	for (i = 0; i < nents; ++i) {
 		dma_map_sg(dev, sg, 1, direction);
+		sg = sg_next(sg);
+	}
 
 	return nents;
 }
@@ -238,8 +240,10 @@ static int qce_dma_unmap_sg(struct device *dev, struct scatterlist *sg,
 {
 	int i;
 
-	for_each_sg(sg, sg, nents, i)
+	for (i = 0; i < nents; ++i) {
 		dma_unmap_sg(dev, sg, 1, direction);
+		sg = sg_next(sg);
+	}
 
 	return nents;
 }
